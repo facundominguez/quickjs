ed7e2f0219acfa5db176d92355f140b4be902c4b
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-05-30 11:02:58 -0300

    Fix typos in nix configuration

--- shell.nix
+++ shell.nix
@@ -3,9 +3,5 @@
 with pkgs;
 multiStdenv.mkDerivation {
   name = "quickjs_multi";
-  builtInputs = [ gcc gnumake pkgsi686Linux.valgrind ];
-  # valgrind is missing from the path for some reason
-  shellHook = ''
-    export PATH="${pkgsi686Linux.valgrind}/bin:$PATH"
-    '';
+  buildInputs = [ gcc gnumake pkgsi686Linux.valgrind ];
 }
--- shell32.nix
+++ shell32.nix
@@ -3,9 +3,5 @@
 with pkgs.pkgsi686Linux;
 stdenv.mkDerivation {
   name = "quickjs_multi";
-  builtInputs = [ gcc gnumake valgrind ];
-  # valgrind is missing from the path for some reason
-  shellHook = ''
-    export PATH="${valgrind}/bin:$PATH"
-    '';
+  buildInputs = [ gcc gnumake valgrind ];
 }
